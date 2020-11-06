335d9d7be1aedb08c2a2a10c748e6ded6f4a387f
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-06 10:24:50 +0100

    build unit

---
+++ Assets/Scripts/Buildings/BuildUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildUnit : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject _unit = null;
+    [SerializeField] private Transform _buildPoint = null;
+
+    #region Server
+    [Command]
+    private void CommandSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(
+            _unit,
+            _buildPoint.position,
+            _buildPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+    #endregion
+
+    #region Client
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) { return; }
+
+        if (!hasAuthority) { return; }
+
+        CommandSpawnUnit();
+    }
+    #endregion
+}
--- Assets/Scripts/Network/CustomNetworkManager.cs
+++ Assets/Scripts/Network/CustomNetworkManager.cs
@@ -7,6 +7,14 @@ namespace MH.Games.RTS
 {
     public class CustomNetworkManager : NetworkManager
     {
-        
+        [SerializeField] private GameObject _unitBuilder = null;
+
+        public override void OnServerAddPlayer(NetworkConnection conectionToTheClient)
+        {
+            base.OnServerAddPlayer(conectionToTheClient);
+            GameObject unitBuilderInstance = 
+                Instantiate(_unitBuilder, conectionToTheClient.identity.transform.position, conectionToTheClient.identity.transform.rotation);
+            NetworkServer.Spawn(unitBuilderInstance, conectionToTheClient);
+        }
     }
 }
