d42f91cb9ad84e47daab423f43b23da9055e4101
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-11-11 10:32:28 +0100

    UI facing camera, layers setting

---
+++ Assets/Scripts/UI/FacingCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MH.Games.RTS
+{
+    public class FacingCamera : MonoBehaviour
+    {
+        private Transform _mainCameraTransform;
+
+        private void Start()
+        {
+            _mainCameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(
+                transform.position + _mainCameraTransform.rotation * Vector3.forward,
+                _mainCameraTransform.rotation * Vector3.up
+                );
+        }
+    }
+}
+
